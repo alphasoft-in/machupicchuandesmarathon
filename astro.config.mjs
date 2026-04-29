559b058841fc57776ecc45a7f1eebc5da18e1fd6
Rubén Tipiani <38152657+rtipiani@users.noreply.github.com>
2026-04-29 16:02:07 -0500

    fix: revert to non-www domain globally

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.peruandestrekking.com',
+  site: 'https://peruandestrekking.com',
   integrations: [react(), sitemap({
     changefreq: 'weekly',
     priority: 0.7,
